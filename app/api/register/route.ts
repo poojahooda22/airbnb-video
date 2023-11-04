ae84833f80936d39dcd44b2a1aecb3b2e6a39a89
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-04 14:14:38 +0530

    update route

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -2,23 +2,31 @@ import bcrypt from 'bcrypt';
 import prisma from '@/app/libs/prismadb';
 import { NextResponse } from 'next/server';
 
-export async function POST (
-    request: Request
-) {
-    const body = await request.json();
-    const { 
-        name, email, password
-     } = body;
+export async function POST(request: Request) {
+  const body = await request.json();
+  const { name, email, password } = body;
 
-    const hashedPassword = await bcrypt.hash(password, 12);
+  // Check if a user with the same email already exists
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
 
-    const user = await prisma.user.create({
-        data: {
-            name,
-            email,
-            hashedPassword
-        }
-    });
+  if (existingUser) {
+    // If user already exists, return an error response
+    return NextResponse.json({ error: 'A user with this email already exists.' });
+  }
 
-    return NextResponse.json(user);
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json(user);
 }
