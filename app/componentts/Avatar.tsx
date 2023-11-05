cb077d9d97031ea355b19f50f18d4aa716340e56
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-05 07:06:08 +0530

    update avatar

--- app/componentts/Avatar.tsx
+++ app/componentts/Avatar.tsx
@@ -2,15 +2,20 @@
 
 import Image from "next/image";
 
+interface AvatarProps {
+  src?: string | null | undefined
+}
 
-const Avatar  = () => {
+const Avatar: React.FC<AvatarProps> = ({
+  src
+}) => {
   return ( 
     <Image 
       className="rounded-full" 
       height="30" 
       width="30" 
       alt="Avatar" 
-      src='/images/placeholder.jpg'
+      src={src || "/images/placeholder.jpg"}
     />
    );
 }
