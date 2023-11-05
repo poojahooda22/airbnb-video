0ffb3a12fc6b56d5867d1e6af2b9135f0aa1633e
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-05 07:12:04 +0530

    update menu

--- app/componentts/navbar/UserMenu.tsx
+++ app/componentts/navbar/UserMenu.tsx
@@ -46,12 +46,20 @@ const UserMenu: React.FC<UserMenuProps> = ({
     };
   }, [handleClickOutside]);
 
+  const onRent = useCallback(() => {
+    if(!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    //open rent modal
+  }, [currentUser, loginModal]);
+
 
   return ( 
     <div className="relative">
       <div className="flex flex-row items-center gap-3">
         <div 
-            onClick={() => {}}
+            onClick={onRent}
           className="
             hidden
             md:block
