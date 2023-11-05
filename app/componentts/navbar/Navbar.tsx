63e6267ff4c131487c0d5909f193c953e56cf191
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-05 07:07:05 +0530

    update navbar

--- app/componentts/navbar/Navbar.tsx
+++ app/componentts/navbar/Navbar.tsx
@@ -1,13 +1,14 @@
 'use client'
 
-import { User } from "@prisma/client";
 import Container from "../Container";
+import Categories from "./Categories";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
+import { SafeUser } from "@/app/types";
 
 interface NavbarProps {
-    currentUser?: User | null;
+    currentUser?: SafeUser | null;
 }
 
 const Navbar: React.FC<NavbarProps> = ({
@@ -38,6 +39,7 @@ const Navbar: React.FC<NavbarProps> = ({
                     </div>
                 </Container>
             </div>
+            <Categories />
         </div>
      );
 }
