089a89c889f8291e62325bd5bfa042d130a74207
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-06 11:28:46 +0530

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,9 @@ export default async function RootLayout({
           <RegisterModal   />
           <Navbar currentUser={currentUser} />
         </ClientOnly>
-        {children}
+        <div className='pb-20 pt-28'>
+          {children}
+        </div>
         </body>
     </html>
   )
