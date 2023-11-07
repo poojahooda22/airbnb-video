0d591ad463bed4059bf259dfcb9414f060159784
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-08 04:46:16 +0530

    update search

--- app/componentts/navbar/Search.tsx
+++ app/componentts/navbar/Search.tsx
@@ -3,25 +3,57 @@
 import { useSearchParams } from 'next/navigation';
 import { useMemo } from 'react';
 import { BiSearch } from 'react-icons/bi';
-// import { differenceInDays } from 'date-fns';
+import { differenceInDays } from 'date-fns';
 
-// import useSearchModal from '@/app/hooks/useSearchModal';
-// import useCountries from '@/app/hooks/useCountries';
+import useSearchModal from '@/app/hooks/useSearchModal';
+import useCountries from '@/app/hooks/useCountries';
 
 const Search = () => {
-//   const searchModal = useSearchModal();
+  const searchModal = useSearchModal();
   const params = useSearchParams();
+  const { getByValue } = useCountries();
 
 
-  // const  locationValue = params?.get('locationValue'); 
-  // const  startDate = params?.get('startDate');
-  // const  endDate = params?.get('endDate');
-  // const  guestCount = params?.get('guestCount');
+  const  locationValue = params?.get('locationValue'); 
+  const  startDate = params?.get('startDate');
+  const  endDate = params?.get('endDate');
+  const  guestCount = params?.get('guestCount');
 
+
+  const  locationLabel = useMemo(() => {
+    if(locationValue) {
+      return getByValue(locationValue as string)?.label;
+    }
+    return 'Anywhere';
+  }, [getByValue, locationValue]);
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string);
+      const end = new Date(endDate as string);
+      let diff = differenceInDays(end, start);
+
+      if (diff === 0) {
+        diff = 1;
+      }
+
+      return `${diff} Days`;
+    }
+
+    return 'Any Week'
+  }, [startDate, endDate]);
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`;
+    }
+
+    return 'Add Guests';
+  }, [guestCount]);
 
   return ( 
     <div
-    
+      onClick={searchModal.onOpen}  
       className="
         border-[1px] 
         w-full 
@@ -49,7 +81,7 @@ const Search = () => {
             px-6
           "
         >
-          Anywhere
+          {locationLabel}
         </div>
         <div 
           className="
@@ -63,7 +95,7 @@ const Search = () => {
             text-center
           "
         >
-          Any Week
+         { durationLabel}
         </div>
         <div 
           className="
@@ -77,7 +109,7 @@ const Search = () => {
             gap-3
           "
         >
-          <div className="hidden sm:block">Add Guests</div>
+          <div className="hidden sm:block">{guestLabel}</div>
           <div 
             className="
               p-2 
