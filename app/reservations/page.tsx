771874daa3fc7c39466002f4c7e47fac8e8160cb
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-08 04:49:24 +0530

    create page

---
+++ app/reservations/page.tsx
@@ -0,0 +1,48 @@
+
+import EmptyState from "@/app/componentts/EmptyState";
+import ClientOnly from "@/app/componentts/ClientOnly";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import getReservations from "@/app/actions/getReservations";
+
+import TripsClient from "./ReservationsClient";
+import ReservationsClient from "./ReservationsClient";
+
+const ReservationsPage = async () => {
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return (
+      <ClientOnly> 
+        <EmptyState
+          title="Unauthorized"
+          subtitle="Please login"
+        />
+      </ClientOnly>
+    )
+  }
+
+  const reservations = await getReservations({ authorId: currentUser.id });
+
+  if (reservations.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No reservations found"
+          subtitle="Looks like you have no reservations on your properties."
+        />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <ReservationsClient
+        reservations={reservations}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  );
+}
+ 
+export default ReservationsPage;
