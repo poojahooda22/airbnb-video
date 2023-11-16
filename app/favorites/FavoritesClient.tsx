0f507d29f71515c6c61232e06229241085d915c8
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-16 12:28:24 +0530

    updated

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -32,7 +32,7 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({
           gap-8
         "
       >
-        {listings.map((listing: any) => (
+        {listings.map((listing) => (
           <ListingCard
             currentUser={currentUser}
             key={listing.id}
