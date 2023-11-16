8fbd14467de5e42cae804d6ff68e81e9a3c7bc00
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-16 12:28:18 +0530

    updated

--- app/componentts/navbar/Navbar.tsx
+++ app/componentts/navbar/Navbar.tsx
@@ -8,7 +8,7 @@ import UserMenu from "./UserMenu";
 import { SafeUser } from "@/app/types";
 
 interface NavbarProps {
-    currentUser?: SafeUser | null;
+    currentUser?: SafeUser | null | undefined;
 }
 
 const Navbar: React.FC<NavbarProps> = ({
