4a5fa94968145cc9af0779370d27bd4f178ac35a
pooja <91055527+poojahooda22@users.noreply.github.com>
2023-11-17 14:53:13 +0530

    updated page.tsx

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,38 +1,40 @@
-import getListingById from "@/app/actions/getListingById";
-import ClientOnly from "@/app/componentts/ClientOnly";
-import EmptyState from "@/app/componentts/EmptyState";
+
 import getCurrentUser from "@/app/actions/getCurrentUser";
-import ListingClient from "./ListingClient";
+import getListingById from "@/app/actions/getListingById";
 import getReservations from "@/app/actions/getReservations";
 
+import ClientOnly from "@/app/componentts/ClientOnly";
+import EmptyState from "@/app/componentts/EmptyState";
+
+import ListingClient from "./ListingClient";
 
 interface IParams {
-    listingId?: string;
+  listingId?: string;
 }
- 
 
+const ListingPage = async ({ params }: { params: IParams }) => {
 
-const ListingPage = async ({ params }: {params: IParams}) => {
-    const listing= await getListingById(params);
-    const currentUser = await getCurrentUser();
-    const reservations = await getReservations(params);
+  const listing = await getListingById(params);
+  const reservations = await getReservations(params);
+  const currentUser = await getCurrentUser();
 
-    if(!listing) {
-        return (
-            <ClientOnly>
-                <EmptyState />
-            </ClientOnly>
-        )
-    }
-    return ( 
-        <ClientOnly>
-            <ListingClient
-                listing={listing}
-                reservations={reservations}
-                currentUser={currentUser}
-            />
-        </ClientOnly>
-     );
+  if (!listing) {
+    return (
+      <ClientOnly>
+        <EmptyState />
+      </ClientOnly>
+    );
+  }
+
+  return (
+    <ClientOnly>
+      <ListingClient
+        listing={listing}
+        reservations={reservations}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  );
 }
  
 export default ListingPage;
