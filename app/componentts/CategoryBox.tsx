e75ce0a6d37ad495e32779a59c2cbaa14948778d
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-03-13 13:59:53 +0530

    updated

--- app/componentts/CategoryBox.tsx
+++ app/componentts/CategoryBox.tsx
@@ -57,6 +57,7 @@ const CategoryBox: React.FC<CategoryBoxProps> = ({
           hover:text-neutral-800
           transition
           cursor-pointer
+          
           ${selected ? 'border-b-neutral-800' : 'border-transparent'}
           ${selected ? 'text-neutral-800' : 'text-neutral-500'}
         `}
