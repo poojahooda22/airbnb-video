f145157594224d91c81b0253e3f00178e9b19114
Pooja Hooda <91055527+poojahooda22@users.noreply.github.com>
2024-05-24 21:39:14 +0530

    updated page

--- app/page.tsx
+++ app/page.tsx
@@ -9,42 +9,50 @@ interface HomeProps {
   searchParams: IListingsParams
 }
 
+
 const Home = async ({
   searchParams
 } : HomeProps) => {
-  const listings = await getListings(searchParams);
-  const currentUser = await getCurrentUser();
-  // const isEmpty = true;
 
-  if(listings.length === 0) {
+
+  try {
+    const listings = await getListings(searchParams);
+    const currentUser = await getCurrentUser();
+    // const isEmpty = true;
+
+    if(listings.length === 0) {
+      return (
+        <ClientOnly>
+          <EmptyState  showReset/>
+        </ClientOnly>
+      )
+    }
+
     return (
       <ClientOnly>
-        <EmptyState  showReset/>
+        <Container>
+          <div 
+            className="pt-24 grid grid-cols-1 sm:grid-cols-2 
+            md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 
+            gap-8"
+          >
+            {listings.map((listing) => {
+              return (
+                <ListingCard 
+                  currentUser={currentUser} 
+                  data={listing} 
+                  key={listing.id} 
+                />
+              )
+            })}
+          </div>
+        </Container>
       </ClientOnly>
     )
   }
-
-  return (
-    <ClientOnly>
-      <Container>
-        <div 
-          className="pt-24 grid grid-cols-1 sm:grid-cols-2 
-          md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 
-          gap-8"
-        >
-          {listings.map((listing) => {
-            return (
-              <ListingCard 
-                currentUser={currentUser} 
-                data={listing} 
-                key={listing.id} 
-              />
-            )
-          })}
-        </div>
-      </Container>
-    </ClientOnly>
-  )
+  catch (err) {
+    return;
+  }
 }
 
 export default Home;
